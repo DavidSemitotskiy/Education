8c237aa10a57ce8e6cd57928761759a5ad3e2db8
DavidSemitotskiy <davidsemitotskiy@gmail.com>
2022-07-09 16:41:29 +0300

    SRP without correction

---
+++ SRP/SRP/User.cs
@@ -0,0 +1,39 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.IO;
+
+namespace SRP
+{
+    public class User
+    {
+        private IUserStore store;
+
+        public User(IUserStore store)
+        {
+            this.store = store;
+        }
+
+        private string name;
+
+        public string Name
+        {
+            get => this.name;
+            set
+            {
+                var userExists = this.store.FindUserByName(value) != null;
+
+                if (userExists)
+                {
+                    throw new Exception($"The name {value} is already taken");
+                }
+
+                this.name = value;
+            }
+        }
+
+        public void SaveToFile(string fileName)
+        {
+            File.WriteAllText(fileName, JsonConvert.SerializeObject(this));
+        }
+    }
+}
