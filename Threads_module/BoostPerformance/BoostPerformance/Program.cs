06f198bb7a1425083d9e2d771855995adc3a7ba7
DavidSemitotskiy <davidsemitotskiy@gmail.com>
2022-07-28 17:03:47 +0300

    initial program without boost performance

---
+++ Threads_module/BoostPerformance/BoostPerformance/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BoostPerformance
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            Random r = new Random();
+
+            decimal[] arr = Enumerable.Range(0, 1000000).Select(_ => (decimal)r.NextDouble()).ToArray();
+            decimal sum = 0;
+
+            var sw = Stopwatch.StartNew();
+
+            foreach (var item in arr)
+            {
+                sum += item;
+            }
+
+            sw.Stop();
+            Console.WriteLine(sw.ElapsedMilliseconds);
+        }
+    }
+}
