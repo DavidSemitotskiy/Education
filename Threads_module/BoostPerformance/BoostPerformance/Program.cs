dfa6dc65198fa4da03e23e932edfda44b08e4ef2
DavidSemitotskiy <davidsemitotskiy@gmail.com>
2022-07-28 22:09:23 +0300

    add threads to sum elements of array

--- Threads_module/BoostPerformance/BoostPerformance/Program.cs
+++ Threads_module/BoostPerformance/BoostPerformance/Program.cs
@@ -5,26 +5,51 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace BoostPerformance
+namespace Threads
 {
     public class Program
     {
         public static void Main()
         {
+            //without performance: 76 - 92ms
+            //after boost 35 - 42ms
             Random r = new Random();
 
             decimal[] arr = Enumerable.Range(0, 1000000).Select(_ => (decimal)r.NextDouble()).ToArray();
             decimal sum = 0;
-
+            List<Thread> listThreads = new List<Thread>();
+            List<decimal> sumsThreads = new List<decimal>();
             var sw = Stopwatch.StartNew();
-
-            foreach (var item in arr)
+            for (int i = 0; i < 4; i++)
             {
-                sum += item;
+                int index = i;
+                listThreads.Add(new Thread(() =>
+                {
+                    decimal sumThread = 0;
+                    int start = index * 250000;
+                    int end = start + 250000;
+                    for (int j = start; j < end; j++)
+                    {
+                        sumThread += arr[j];
+                    }
+
+                    lock (sumsThreads)
+                    {
+                        sumsThreads.Add(sumThread);
+                    }
+                }));
+
+                listThreads[i].Start();
+            }
+
+            for (int i = 0; i < listThreads.Count; i++)
+            {
+                listThreads[i].Join();
             }
 
             sw.Stop();
             Console.WriteLine(sw.ElapsedMilliseconds);
+            Console.WriteLine(sumsThreads.Sum());
         }
     }
 }
