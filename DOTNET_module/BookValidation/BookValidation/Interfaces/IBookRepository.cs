ae799dd3c28f54317fb762c87f8257bee35706f2
DavidSemitotskiy <davidsemitotskiy@gmail.com>
2022-07-29 09:44:50 +0300

    add interfaces for book service and book repository

---
+++ DOTNET_module/BookValidation/BookValidation/Interfaces/IBookRepository.cs
@@ -0,0 +1,16 @@
+﻿using BookValidation.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BookValidation.Interfaces
+{
+    public interface IBookRepository
+    {
+        void SaveEncryptBook(EncryptedBook encryptedBook);
+
+        IEnumerable<Book> GetAllBooks();
+    }
+}
