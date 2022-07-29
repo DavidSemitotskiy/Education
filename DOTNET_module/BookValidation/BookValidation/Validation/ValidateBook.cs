9650a8b997d27034b09f4d1f6f5d6bd01e3a6248
DavidSemitotskiy <davidsemitotskiy@gmail.com>
2022-07-29 10:12:14 +0300

    add class for validate inputted string

---
+++ DOTNET_module/BookValidation/BookValidation/Validation/ValidateBook.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BookValidation.Validation
+{
+    public static class ValidateBook
+    {
+        private static string _pattern = "^([A-Za-zA-Яа-яІ-і .,\']+) - ([A-Za-zA-Яа-яІ-і .,:()\'\"]+), (\\d{1,4}) \\(((?:\\d{2}(?:\\.|/)\\d{2}(?:\\.|/)\\d{4}|\\d{4}))\\)\\.(pdf|doc|txt)";
+    }
+}
