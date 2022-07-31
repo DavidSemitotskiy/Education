b3d1538442dc68a1ebe96a5ab090eaf9008c408c
DavidSemitotskiy <davidsemitotskiy@gmail.com>
2022-07-31 11:04:59 +0300

    change regex pattern for validation book

--- DOTNET_module/BookValidation/BookValidation/Validation/ValidateBook.cs
+++ DOTNET_module/BookValidation/BookValidation/Validation/ValidateBook.cs
@@ -9,7 +9,7 @@ namespace BookValidation.Validation
 {
     public class ValidateBook
     {
-        private string _pattern = "^([A-Za-zA-Яа-яІ-і .,\']+) - ([A-Za-zA-Яа-яІ-і .,:()\'\"]+), (\\d{1,4}) \\(((?:\\d{2}(?:\\.|/)\\d{2}(?:\\.|/)\\d{4}|\\d{4}))\\)\\.(pdf|doc|txt)";
+        private string _pattern = "^([A-Za-zA-Яа-яІ-і .,\']+) - ([A-Za-zA-Яа-яІ-і .,:()\'\"]+), (\\d{1,4}) \\((?:((?:\\d{2}\\.\\d{2}\\.\\d{4})|(?:\\d{2}/\\d{2}/\\d{4})|\\d{4}))\\)\\.(pdf|doc|txt)";
 
         public Match IsValidInputString(string inputtedString)
         {
