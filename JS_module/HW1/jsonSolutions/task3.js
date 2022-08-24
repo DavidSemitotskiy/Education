a29dbf887693d196bc75efdb9c895d41baa2cbb5
DavidSemitotskiy <davidsemitotskiy@gmail.com>
2022-08-24 14:10:15 +0300

    complete fourth task

---
+++ JS_module/HW1/jsonSolutions/task3.js
@@ -0,0 +1,18 @@
+const randomNumber = Math.floor(Math.random() * (20 - 1) + 1);
+while (true)
+{
+  let userNumber = +prompt("Input Number"); 
+  if (userNumber == randomNumber)
+  {
+    alert("You picked correct number!!!");
+    break;
+  }
+  else if (userNumber > randomNumber)
+  {
+    alert("Hidden number is lower than inputted one");
+  }
+  else
+  {
+    alert("Hidden number is bigger than inputted one");
+  }
+}
