070699564f10f5e874c512c97bd16be166582d6e
DavidSemitotskiy <davidsemitotskiy@gmail.com>
2022-08-24 14:25:28 +0300

    completed fifth task

---
+++ JS_module/HW1/jsonSolutions/task5.js
@@ -0,0 +1,11 @@
+function addSymbolsToStr(str)
+{
+  if (str.startsWith("Py")){
+    return str;
+  }
+
+  return str.replace(str, "Py" + str);
+}
+
+let inputtedString = prompt("Input string");
+alert(addSymbolsToStr(inputtedString));
