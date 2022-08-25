ce56b6381e20708c4b3ad228d11940131de1786a
DavidSemitotskiy <davidsemitotskiy@gmail.com>
2022-08-25 13:16:00 +0300

    add date to html

--- JS_module/HW1/jsonSolutions/task1.js
+++ JS_module/HW1/jsonSolutions/task1.js
@@ -1,9 +1,13 @@
-function date(){
+function date()
+{
   let date = new Date();
   let hours = date.getHours() < 10 ? "0" + date.getHours() : date.getHours();
   let minutes = date.getMinutes() < 10 ? "0" + date.getMinutes() : date.getMinutes();
   let seconds = date.getSeconds() < 10 ? "0" + date.getSeconds() : date.getSeconds();
+  let day = date.getDate() < 10 ? "0" + date.getDate() : date.getDate();
+  let month = date.getMonth() + 1 < 10 ? "0" + (date.getMonth() + 1) : date.getMonth() + 1;
+  let year = date.getFullYear();
   let element = document.getElementById("time");
-  element.innerHTML = `${hours}:${minutes}:${seconds}`;
+  element.innerHTML = `${day}.${month}.${year} - ${hours}:${minutes}:${seconds}`;
 }
 setInterval(date, 1000);
