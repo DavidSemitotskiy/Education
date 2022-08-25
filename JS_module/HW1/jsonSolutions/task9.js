3f1b444cd63258fbf768e7c104c9fc215afbb2bb
DavidSemitotskiy <davidsemitotskiy@gmail.com>
2022-08-25 14:31:19 +0300

    formated brackets

--- JS_module/HW1/jsonSolutions/task9.js
+++ JS_module/HW1/jsonSolutions/task9.js
@@ -1,8 +1,10 @@
 function combine(arr, separator)
 {
-  if (arr instanceof Array){
+  if (arr instanceof Array)
+  {
     let resultStr = String();
-    for (let index = 0; index < arr.length; index++){
+    for (let index = 0; index < arr.length; index++)
+    {
       resultStr = index + 1 == arr.length ? resultStr.concat(String(arr[index])) : resultStr.concat(String(arr[index]).concat(String(separator)));
     }
   
