1f73756832bd93de2bc57f137f244377ccadc645
DavidSemitotskiy <davidsemitotskiy@gmail.com>
2022-09-13 10:02:27 +0300

    Add context to operate with database and create initial migration migration

---
+++ ASP.NET_Core_MVC_module/ContactsWeb/ContactsWeb/Data/ApplicationContext.cs
@@ -0,0 +1,14 @@
+﻿using ContactsWeb.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace ContactsWeb.Data
+{
+    public class ApplicationContext : DbContext
+    {
+        public ApplicationContext(DbContextOptions<ApplicationContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Contact> Contacts { get; set; }
+    }
+}
--- ASP.NET_Core_MVC_module/ContactsWeb/ContactsWeb/Program.cs
+++ ASP.NET_Core_MVC_module/ContactsWeb/ContactsWeb/Program.cs
@@ -1,6 +1,14 @@
+using ContactsWeb.Data;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+builder.Services.AddDbContext<ApplicationContext>(options =>
+{
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
+});
+
 builder.Services.AddControllersWithViews();
 
 var app = builder.Build();
