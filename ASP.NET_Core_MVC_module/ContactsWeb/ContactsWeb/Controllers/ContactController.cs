481db9ad4ae7ede2e1455caf8adb98fe70965bad
DavidSemitotskiy <davidsemitotskiy@gmail.com>
2022-09-13 11:56:09 +0300

    add partial for add and edit forms and implement add action in ContactController

---
+++ ASP.NET_Core_MVC_module/ContactsWeb/ContactsWeb/Controllers/ContactController.cs
@@ -0,0 +1,47 @@
+﻿using ContactsWeb.Entities;
+using ContactsWeb.Models;
+using ContactsWeb.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ContactsWeb.Controllers
+{
+    public class ContactController : Controller
+    {
+        private readonly IContactManager _contactManager;
+
+        public ContactController(IContactManager contactManager)
+        {
+            _contactManager = contactManager;
+        }
+
+        public IActionResult Add()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Add(ContactViewModel contact)
+        {
+            if (ModelState.IsValid)
+            {
+                var newContact = new Contact
+                {
+                    Id = Guid.NewGuid(),
+                    Name = contact.Name,
+                    Number = contact.Number,
+                };
+                if (await _contactManager.ExistsAsync(newContact))
+                {
+                    ModelState.AddModelError("Name", "This contact already exists");
+                    return View(contact);
+                }
+
+                await _contactManager.AddAsync(newContact);
+                await _contactManager.SaveChangesAsync();
+                return RedirectToAction("Index", "Home");
+            }
+
+            return View(contact);
+        }
+    }
+}
---
+++ ASP.NET_Core_MVC_module/ContactsWeb/ContactsWeb/Models/ContactViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ContactsWeb.Models
+{
+    public class ContactViewModel
+    {
+        [Required(ErrorMessage = "This is a required field")]
+        public string Name { get; set; }
+
+        [Required(ErrorMessage = "This is a required field")]
+        public string Number { get; set; }
+    }
+}
